c9be165d5e61e11f4587804b1f45255883f53a9d
dev-yuth <dev.yuth@gmail.com>
2025-10-07 23:34:45 +0700

    refactor : manage.py comment print debug

--- manage.py
+++ manage.py
@@ -5,6 +5,6 @@ from flask_migrate import Migrate
 
 app = create_app()
 migrate = Migrate(app, db)
-print("manage.py imported - creating app and migrate")
+# print("manage.py imported - creating app and migrate")
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
